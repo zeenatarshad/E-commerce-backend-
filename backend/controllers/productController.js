73091bd271187da2febc19a5253ca51b0b3aa5e7
zeenatarshad <zputhawala3011@gmail.com>
2023-03-29 12:24:29 +0530

    adding serch filter and pagination

---
+++ backend/controllers/productController.js
@@ -0,0 +1,84 @@
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+
+// Create Product --> ADMIN
+exports.createProduct = catchAsyncErrors(async(req,res,next) => {
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+});
+
+// Get all Products 
+exports.getAllProducts = catchAsyncErrors(async(req,res) =>{
+
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
+
+    const products = await apiFeature.query;
+    res.status(200).json({
+        success:true,
+        products
+    })
+});
+
+// Get a Product details
+exports.getProductDetails = catchAsyncErrors(async(req,res,next) =>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found",404));
+    }
+
+    res.status(200).json({
+        success:true,
+        product,
+        productCount
+    })
+});
+
+// Update Product --> ADMIN
+exports.updateProduct = catchAsyncErrors(async(req,res,next) =>{
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found",404));
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+});
+
+// Delete Product --> ADMIN
+exports.deleteProduct = catchAsyncErrors(async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found",404));
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        message:"Product deleted successfully!"
+    })
+}); 
