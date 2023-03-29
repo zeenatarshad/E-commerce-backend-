c087c096e26f610598f57affdba6cc7587e6aab4
zeenatarshad <zputhawala3011@gmail.com>
2023-03-29 23:18:59 +0530

    generating user token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
 
@@ -39,5 +41,22 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date
 });
+
+// Encrypting Password
+userSchema.pre("save",async function(next){
+
+    if(!this.isModified("password")){
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password,10); 
+});
+
+// JWT Token
+userSchema.methods.getJWTToken = function(){
+    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
 
 module.exports = mongoose.model("User",userSchema);
