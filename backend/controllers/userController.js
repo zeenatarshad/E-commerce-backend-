4c32b1c1a7f5b8fa9b9c45db31f814bad14e9187
zeenatarshad <zputhawala3011@gmail.com>
2023-03-31 05:45:56 +0530

    reset password link email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async(req,res,next) => {
@@ -59,4 +60,46 @@ exports.logout = catchAsyncErrors(async (req,res,next) => {
         success: true,
         message: "Logged out successfully!"
     });
+});
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async(req,res,next)=>  {
+
+    const user = await User.findOne({email: req.body.email});
+
+    if(!user){
+        return next(new ErrorHandler("User not found",404));
+    }
+
+    // Get ResetPassword Token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false});
+
+    const resetPasswordUrl = `${req.protocol}://${req.get(
+        "host"
+    )}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, kindly ignore it`;
+
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: `Apollo Tradelink Password Recovery`,
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`,
+        });
+
+    } catch(error){
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false});
+
+        return next(new ErrorHandler(error.message, 500));
+    }
 });
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,10 +3,10 @@ const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductD
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser,createProduct);
-router.route("/product/:id").put(isAuthenticatedUser,updateProduct);
-router.route("/product/:id").delete(isAuthenticatedUser,deleteProduct);
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
+router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct);
+router.route("/product/:id").delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
 router.route("/product/:id").get(getProductDetails);
 
-module.exports = router
+module.exports = router;
