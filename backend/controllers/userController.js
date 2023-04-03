bb638b423a4b42083ec5effbac01ff4b4babf894
zeenatarshad <zputhawala3011@gmail.com>
2023-04-03 12:33:14 +0530

    getting user details by admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -163,3 +163,53 @@ exports.updatePassword = catchAsyncErrors(async(req,res,next) => {
 
     sendToken(user,200,res);
 });
+
+// Update User Profile
+exports.updateProfile = catchAsyncErrors(async(req,res,next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    // we will add cloudinary later
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      });
+    
+      res.status(200).json({
+        success: true,
+      });
+});
+
+// Get all users  --> ADMIN
+exports.getAllUsers = catchAsyncErrors(async(req,res,next) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users,
+    });
+});
+
+// Get single user  --> ADMIN
+exports.getSingleUser = catchAsyncErrors(async(req,res,next) => {
+    const user = await User.findById(req.params.id);
+
+    if(!user){
+        return next(
+            new ErrorHandler(`User doesn't exist with id: ${req.params.id}`,404)
+        );
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+
+
+
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
@@ -10,5 +10,8 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
 
 module.exports = router;
